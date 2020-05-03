e4db52b37a7569f042bb6c36c592068deff92d06
mohamedbrahimi <mohkriss3@gmail.com>
2020-05-04 00:57:31 +0100

    review and refactor the code.

--- server-b/app/controller/getSimpleData.controller.js
+++ server-b/app/controller/getSimpleData.controller.js
@@ -1,5 +1,20 @@
-function getSimpleData(client) {
-    return new Promise((resolve, reject) => {
+import gRPC from "grpc";
+const  protoLoader = require('@grpc/proto-loader');
+
+async function requestData(req, res, next) {
+
+    const packageDefinition = protoLoader.loadSync('.porto', {
+        keepCase: true,
+        longs: String,
+        enums: String,
+        defaults: true,
+        oneofs: true
+    });
+    const packageObject = gRPC.loadPackageDefinition(packageDefinition);
+
+    const client = new packageObject.SampleDataService('localhost:8001', gRPC.credentials.createInsecure());
+
+    const response = await new Promise((resolve, reject) => {
         client.GetSampleData({}, (err, response) => {
             console.log('err', err);
             if (!response?.err) {
@@ -9,9 +24,10 @@ function getSimpleData(client) {
             }
         });
     });
+
+    res.json(response);
 }
 
-
 export {
-    getSimpleData,
+    requestData,
 }
